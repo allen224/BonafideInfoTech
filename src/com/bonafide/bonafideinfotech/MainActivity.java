31c947e9ea36ef81a68809729be083fba137b8fa
allen224 <crisallencenidoza@yahoo.com.ph>
2014-07-23 17:44:22 +0800

    about us layout added

--- src/com/bonafide/bonafideinfotech/MainActivity.java
+++ src/com/bonafide/bonafideinfotech/MainActivity.java
@@ -1,9 +1,12 @@
 package com.bonafide.bonafideinfotech;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        TextView tv = (TextView)findViewById(R.id.about_us);
+        
+        tv.setOnClickListener(new View.OnClickListener() {
+        	   @Override
+        	   public void onClick(View v) {
+        	      // TODO Auto-generated method stub
+        		   Intent callIntent = new Intent(MainActivity.this, About_us.class);
+        		   startActivity(callIntent);
+        	   }
+        	});
     }
 
 
